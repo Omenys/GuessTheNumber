8b823eb2b716c762a9d55ef457c6b307fea91ce7
Omenys <105769289+Omenys@users.noreply.github.com>
2024-02-10 08:02:55 -0600

    Logic added - needs work

--- Assets/GuessTheNumber.cs
+++ Assets/GuessTheNumber.cs
@@ -1,11 +1,17 @@
+using TMPro;
 using UnityEngine;
 
 public class GuessTheNumber : MonoBehaviour
 {
+    [SerializeField] TMP_Text header;
+    [SerializeField] TMP_Text inputField;
+    int number = 0;
+    int numberOfGuesses = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        GameSetup();
     }
 
     // Update is called once per frame
@@ -14,18 +20,43 @@ public class GuessTheNumber : MonoBehaviour
 
     }
 
-    void GameSetup()
+    public void ChangeHeader(string text)
     {
-        // Choose the random number that the player must guess
-        // Update the header text to let the player know how many attempts are remaining
-        // Clear the text in the input field
+        header.text = text;
     }
 
-    void SubmitGuess()
+    public void GameSetup()
     {
-        // Check if the number in the input field is correct
-        // Update header text to let them know they are wrong
-        // Decrease remaining attempts
-        // Update header text to let them know they have won
+        // Choose the random number that the player must guess (1-10)
+        number = Random.Range(1, 11);
+
+        // Guesses remaining
+        numberOfGuesses = 3;
+
+        // Update the header text to let the player know how many attempts are remaining
+        header.text = numberOfGuesses.ToString() + " guesses remaining";
+        // Clear the text in the input field
+        inputField.text = "Enter your guess...";
     }
+
+    public void SubmitGuess()
+    {
+        // Store user input
+        int input = 0;
+
+        // Update header text if correct
+        if (input == number)
+        {
+            header.text = "You won!";
+        }
+        // Update header text if incorrect
+        else
+        {
+            header.text = "Try again!";
+        }
+        // Decrease remaining attempts
+        numberOfGuesses = numberOfGuesses - 1;
+
+    }
+
 }
